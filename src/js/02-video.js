2c7383011306103a6455b5f4388c486568fe7279
Balinskiy <3879342@gmail.com>
2023-10-09 12:17:53 +0300

    upd-task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const setLocalStorageThrottled = throttle(function (currentTime) {
+    localStorage.setItem(CURRENT_TIME_KEY, currentTime);
+}, 1000); 
+
+player.on('timeupdate', function (data) {
+    const currentTime = data.seconds;
+    console.log('Current time:', currentTime);
+    setLocalStorageThrottled(currentTime);
+});
+
+const savedTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+if (savedTime !== null) {
+    player.setCurrentTime(parseFloat(savedTime));
+}
